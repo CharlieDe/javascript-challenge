c92cdf31103ad8cf73422699fea802fafd992ea2
CharlieDe <charlito28art@gmail.com>
2019-09-06 12:03:04 -0500

    updated

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,13 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-tableData.forEach((AlienReport) => {
-    var row = tbody.append("tr");
-    Object.entries(AlienReport).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
+
   
   // Search through date/time
   var button = d3.select("#filter-btn");
@@ -21,6 +15,14 @@ tableData.forEach((AlienReport) => {
       console.log (inputValue);
       var filterTable = tableData.filter(alien => alien.datetime === inputValue);
       console.log(filterTable);
+      filterTable.forEach((FilterReport) => {
+        var row = tbody.append("tr");
+        Object.entries(FilterReport).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+
 
   });
 
